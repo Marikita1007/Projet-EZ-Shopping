f1bfc0445369ab54103e3c950ce72835a894cb69
Marikita1007 <ishinomaki1007@gmail.com>
2023-06-20 21:01:22 +0200

    Added navbar and folders

---
+++ ez_shopping/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './header.scss'
+
+const Header = () => {
+
+    return (
+        <header id="main_header">
+            <h1>EZ Shopping</h1>
+        </header>
+    )
+}
+
+export default Header;

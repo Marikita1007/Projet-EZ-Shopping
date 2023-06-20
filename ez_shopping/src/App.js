e0576e7ea0240d68d072ca6a448266d680f44b4c
Marikita1007 <ishinomaki1007@gmail.com>
2023-06-20 21:25:43 +0200

    Modified nav

--- ez_shopping/src/App.js
+++ ez_shopping/src/App.js
@@ -1,6 +1,11 @@
 import './App.scss';
 import Header from "./components/Header/Header";
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 const App = () => {
 
 
--- ez_shopping/src/components/Header/Header.js
+++ ez_shopping/src/components/Header/Header.js
@@ -3,9 +3,18 @@ import './header.scss'
 const Header = () => {
 
     return (
-        <header id="main_header">
-            <h1>EZ Shopping</h1>
-        </header>
+        <>
+            <header id="main_header">
+                <h1>EZ Shopping</h1>
+                <nav>
+                    <ul>
+                        <li><a href="">Alan Turing</a></li>
+                        <li><a href="">items</a></li>
+                        <li><a href="">Dark Mode</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     )
 }
 

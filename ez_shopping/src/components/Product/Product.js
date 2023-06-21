8dbea4b72704f8bdae8c2029e29a7eb477f922b3
Marikita1007 <ishinomaki1007@gmail.com>
2023-06-21 09:32:52 +0200

    Added bootstrap and scss to show the prducts

--- ez_shopping/src/components/Product/Product.js
+++ ez_shopping/src/components/Product/Product.js
@@ -4,13 +4,18 @@ import './product.scss'
 
 const Product = ({ product }) => {
     return (
-        <div className="product_item">
-            <img src={product.image} alt={"Product Image"} />
-            <div className="product_name">{product.title}</div>
-            <div className="product_name">{product.category}</div>
-            <div className="product_name">{product.price}</div>
-            <div className="product_name">{product.description}</div>
+        <div className="row product_item">
+            <div className="col-md-4">
+                <img src={product.image} alt="Product Image" />
+            </div>
+            <div className="col-md-8">
+                <div className="product_title">{product.title}</div>
+                <div className="product_category">{product.category}</div>
+                <div className="product_price">{product.price}</div>
+                <div className="product_description">{product.description}</div>
+            </div>
         </div>
+
     )
 }
 

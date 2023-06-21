3d9d103da1a4fbef45622b5cfa272c816aee74d6
Marikita1007 <ishinomaki1007@gmail.com>
2023-06-21 11:02:05 +0200

    Added css to show the details of products

--- ez_shopping/src/components/Product/Product.js
+++ ez_shopping/src/components/Product/Product.js
@@ -9,13 +9,19 @@ const Product = ({ product }) => {
                 <img src={product.image} alt="Product Image" />
             </div>
             <div className="col-md-8">
-                <div className="product_title">{product.title}</div>
-                <div className="product_category">{product.category}</div>
-                <div className="product_price">{product.price}</div>
+                <div className="product_info">
+                    <h3 className="product_title">
+                        {product.title} <span className="product_category bg-secondary text-light">{product.category}</span>
+                    </h3>
+                </div>
                 <div className="product_description">{product.description}</div>
+                <div className="product_price">${product.price}</div>
+                <div className="product_quantity">
+                    Quantity: <input type='text' name='quantity' className='quantity'/>
+                </div>
+                <button type="button" className="btn btn-warning add_product">Add to basket</button>
             </div>
         </div>
-
     )
 }
 
